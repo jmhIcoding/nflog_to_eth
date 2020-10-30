04033c213903e7f7368f230400b1af9a2a255b18
jiangminghao <jiangminghao@antiy.cn>
2020-10-30 17:25:06 +0800

    update

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,4 @@
-#include <util.h>
+#include "util.h"
 #include <time.h>
 #include "BaseTool.h"
 
--- src/util.cpp
+++ src/util.cpp
@@ -1,4 +1,4 @@
-#include <util.h>
+#include "util.h"
 #ifdef DEBUG_INFO
 void DbgPrint(int level, void *header)
 {
